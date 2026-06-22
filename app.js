37040d4083619fd428d75ccbf92aab0bfb5d8160
Wael Aloush <waelee2018@gmail.com>
2026-06-22 21:49:29 +0300

    Hostinger: use app.js as entry

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-// Hostinger يتوقع أحياناً app.js كملف افتراضي
-import "./server.js";
+// نقطة الدخول الافتراضية على Hostinger (main + start)
+console.log("[mudad] بدء التشغيل...", { node: process.version, port: process.env.PORT });
+
+import("./server/index.js").catch((err) => {
+  console.error("[mudad] فشل التشغيل:", err);
+  process.exit(1);
+});
